d3fe53728fc1bc1d621d2263a94780b6ac44a09b
jamesrapp <jrrapp2@gmail.com>
2019-11-18 15:04:37 -0600

    Added user service client implementation

---
+++ 016 - Application Layering/Client/IUserServiceClient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using Users.Client.Dtos;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+namespace Users.Client
+{
+    public interface IUserServiceClient
+    {
+        /// <summary>
+        /// Gets a user by id
+        /// </summary>
+        /// <param name="id">The id of the user to get</param>
+        Task<UserDto> GetUserAsync(Guid id);
+
+        /// <summary>
+        /// Adds or updates the given user
+        /// </summary>
+        Task AddOrUpdateUserAsync(UserDto user);
+
+        /// <summary>
+        /// Deletes the specified user
+        /// </summary>
+        /// <param name="id">The id of the user to delete</param>
+        Task DeleteUserAsync(Guid id);
+    }
+}
---
+++ 016 - Application Layering/Client/UserServiceClient.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net.Http;
+using Users.Client.Dtos;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+namespace Users.Client
+{
+    public class UserServiceClient : IUserServiceClient
+    {
+        private readonly string _serviceBaseUrl;
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public UserServiceClient(string serviceBaseUrl, IHttpClientFactory httpClientFactory)
+        {
+            _serviceBaseUrl = serviceBaseUrl;
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<UserDto> GetUserAsync(Guid id)
+        {
+            var httpClient = _httpClientFactory.CreateClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, $"{_serviceBaseUrl}/users/{id}");
+            var response = await httpClient.SendAsync(request);
+            var responseBodyJson = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<UserDto>(responseBodyJson);
+        }
+
+        public async Task AddOrUpdateUserAsync(UserDto user)
+        {
+            var httpClient = _httpClientFactory.CreateClient();
+            var request = new HttpRequestMessage(HttpMethod.Put, $"{_serviceBaseUrl}/users");
+            request.Content = new StringContent(JsonConvert.SerializeObject(user));
+            request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            var response = await httpClient.SendAsync(request);
+
+            if(!response.IsSuccessStatusCode)
+            {
+                throw new Exception($"An error occurred when creating or updating user {user.Id}.");
+            }
+        }
+
+        public async Task DeleteUserAsync(Guid id)
+        {
+            var httpClient = _httpClientFactory.CreateClient();
+            var request = new HttpRequestMessage(HttpMethod.Put, $"{_serviceBaseUrl}/users/{id}");
+
+            var response = await httpClient.SendAsync(request);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception($"An error occurred when creating or deleting user {id}.");
+            }
+        }
+    }
+}
